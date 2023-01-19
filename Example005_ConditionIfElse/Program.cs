be3a8fc721ae9f9c1cc36a46278c8ca7f6211cec
Spectre29 <ruslan.ibragimov.2003@gmail.com>
2023-01-19 20:27:39 +0500

    Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "руслан")
+{
+    Console.WriteLine("Ура, это же Руслан!");
+}
+
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
